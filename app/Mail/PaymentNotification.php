4653fdf8fa4367a26db273107eb513c5bc9dac7c
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-16 21:12:53 +0530

    Add: Payment notification Email

--- app/Mail/PaymentNotification.php
+++ app/Mail/PaymentNotification.php
@@ -39,7 +39,7 @@ class PaymentNotification extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'mail.payment',
         );
     }
 
