21b4db2afbb56e26f0d3a481b1e4bd1157845101
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-16 21:48:58 +0530

    Fix: Added Exception handling

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -33,80 +33,78 @@ class OrderController extends Controller
                 'status' => 500,
                 'message' => 'An error occurred while sending the email: ' . $e->getMessage()
             ], 500);
-        }
+        } finally {
+            $seats = $request->seats;
+            $shedule = $request->shedule_id;
 
+            DB::beginTransaction();
 
-        $seats = $request->seats;
-        $shedule = $request->shedule_id;
-
-        DB::beginTransaction();
-
-        try {
-            $order_id = DB::table('order')->insertGetId([
-                'created_at' => Carbon::now('Asia/Colombo'),
-                'paid_status' => 0,
-                'user_id' => Auth::id(),
-            ]);
-
-            foreach ($seats as $seat) {
-                $seat_id = DB::table('seat')
-                    ->where('seat_no', $seat)
-                    ->value('id');
-
-                $seat_price = DB::table('seat')
-                    ->where('seat_no', $seat)
-                    ->join('seat_type', 'seat_type.id', '=', 'seat.seat_type')
-                    ->value('seat_type.price');
-
-
-                DB::table('reservations')->insert([
-                    'order_id' => $order_id,
-                    'seat_id' => $seat_id,
-                    'shedule_id' => $shedule,
+            try {
+                $order_id = DB::table('order')->insertGetId([
+                    'created_at' => Carbon::now('Asia/Colombo'),
+                    'paid_status' => 0,
                     'user_id' => Auth::id(),
-                    'timestamp' => Carbon::now(),
-                    'price' => $seat_price,
                 ]);
+
+                foreach ($seats as $seat) {
+                    $seat_id = DB::table('seat')
+                        ->where('seat_no', $seat)
+                        ->value('id');
+
+                    $seat_price = DB::table('seat')
+                        ->where('seat_no', $seat)
+                        ->join('seat_type', 'seat_type.id', '=', 'seat.seat_type')
+                        ->value('seat_type.price');
+
+
+                    DB::table('reservations')->insert([
+                        'order_id' => $order_id,
+                        'seat_id' => $seat_id,
+                        'shedule_id' => $shedule,
+                        'user_id' => Auth::id(),
+                        'timestamp' => Carbon::now(),
+                        'price' => $seat_price,
+                    ]);
+                }
+
+                $data = DB::table('reservations')
+                    ->where('order_id', $order_id)
+                    ->join('seat', 'seat.id', '=', 'reservations.seat_id')
+                    ->get();
+
+                $shedule = DB::table('shedule')
+                    ->join('movie', 'movie.id', '=', 'shedule.movie_id')
+                    ->join('screen', 'screen.screen_id', '=', 'shedule.screen_id')
+                    ->join('screen_time', 'screen_time.id', '=', 'shedule.shedule_time')
+                    ->where('shedule_id', '=', $shedule)
+                    ->select('shedule.*', 'screen_time.time', 'movie.*')
+                    ->get();
+
+                $total_amount = DB::table('reservations')
+                    ->where('order_id', $order_id)
+                    ->join('seat', 'seat.id', '=', 'reservations.seat_id')
+                    ->join('seat_type', 'seat_type.id', '=', 'seat.seat_type')
+                    ->sum('seat_type.price');
+
+                DB::commit();
+
+                dispatch(new CheckOrderStatus($order_id))->delay(now()->addMinutes(10));
+
+                return response()->json([
+                    'status' => 200,
+                    'data' => $data,
+                    'shedule' => $shedule,
+                    'total_amount' => $total_amount,
+                    'order_id' => $order_id,
+                ], 200);
+            } catch (\Exception $e) {
+                DB::rollback();
+
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'An error occurred while processing the order: ' . $e->getMessage()
+                ], 500);
             }
-
-            $data = DB::table('reservations')
-                ->where('order_id', $order_id)
-                ->join('seat', 'seat.id', '=', 'reservations.seat_id')
-                ->get();
-
-            $shedule = DB::table('shedule')
-                ->join('movie', 'movie.id', '=', 'shedule.movie_id')
-                ->join('screen', 'screen.screen_id', '=', 'shedule.screen_id')
-                ->join('screen_time', 'screen_time.id', '=', 'shedule.shedule_time')
-                ->where('shedule_id', '=', $shedule)
-                ->select('shedule.*', 'screen_time.time', 'movie.*')
-                ->get();
-
-            $total_amount = DB::table('reservations')
-                ->where('order_id', $order_id)
-                ->join('seat', 'seat.id', '=', 'reservations.seat_id')
-                ->join('seat_type', 'seat_type.id', '=', 'seat.seat_type')
-                ->sum('seat_type.price');
-
-            DB::commit();
-
-            dispatch(new CheckOrderStatus($order_id))->delay(now()->addMinutes(10));
-
-            return response()->json([
-                'status' => 200,
-                'data' => $data,
-                'shedule' => $shedule,
-                'total_amount' => $total_amount,
-                'order_id' => $order_id,
-            ], 200);
-
-        } catch (\Exception $e) {
-            DB::rollback();
-
-            return response()->json([
-                'status' => 500,
-                'message' => 'An error occurred while processing the order: ' . $e->getMessage()
-            ], 500);
         }
     }
 }
