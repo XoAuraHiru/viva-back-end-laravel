8d3a3f9fd9e40a461a728d08a646b8f8c46eb31a
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-19 02:40:26 +0530

    Fix: Missing webhook_secret

--- config/services.php
+++ config/services.php
@@ -33,6 +33,7 @@ return [
     'stripe' => [
         'secret' => env('STRIPE_SECRET'),
         'publishable' => env('STRIPE_PUBLISHABLE'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],    
 
 ];
